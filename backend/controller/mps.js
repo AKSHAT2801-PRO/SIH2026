9471092859d3229be77f54a7b7c3a3d771056d2a
Akshatshukla28 <akshatshukla0106@gmail.com>
2026-08-30 21:18:40 +0530

    Added Search MPs by name

--- backend/controller/mps.js
+++ backend/controller/mps.js
@@ -3,12 +3,18 @@ const getMps = async (req,res)=>{
     try{
         const page = req.query.page
         const limit = req.query.limit
-        const skip = (page - 1) * limit
-
-        const data = await mps.find({})
-        .skip(skip)
-        .limit(limit)
-        res.json(data)
+        const name = req.query.name
+        if (!name){
+            const skip = (page - 1) * limit
+            const data = await mps.find({})
+            .skip(skip)
+            .limit(limit)
+            res.json(data)
+        }
+        else{
+            const data = await mps.find({mpName : name})
+            res.json(data)
+        }
     }
     catch(error){
         console.log(error)
